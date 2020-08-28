a3d2a261f59983838e3ed5f01d90f18352e6a421
Asherah Connor <ashe@kivikakk.ee>
2020-08-28 17:26:01 +1000

    adjust examples, README template

--- example/comptime-clap.zig
+++ example/comptime-clap.zig
@@ -9,10 +9,11 @@ pub fn main() !void {
     // First we specify what parameters our program can take.
     // We can use `parseParam` to parse a string to a `Param(Help)`
     const params = comptime [_]clap.Param(clap.Help){
-        clap.parseParam("-h, --help          Display this help and exit.              ") catch unreachable,
-        clap.parseParam("-n, --number <NUM>  An option parameter, which takes a value.") catch unreachable,
+        clap.parseParam("-h, --help             Display this help and exit.              ") catch unreachable,
+        clap.parseParam("-n, --number <NUM>     An option parameter, which takes a value.") catch unreachable,
+        clap.parseParam("-s, --string <STR>...  An option parameter which can be specified multiple times.") catch unreachable,
         clap.Param(clap.Help){
-            .takes_value = true,
+            .takes_value = .One,
         },
     };
 
@@ -29,6 +30,8 @@ pub fn main() !void {
         debug.warn("--help\n", .{});
     if (args.option("--number")) |n|
         debug.warn("--number = {}\n", .{n});
+    for (args.options("--string")) |s|
+        debug.warn("--string = {}\n", .{s});
     for (args.positionals()) |pos|
         debug.warn("{}\n", .{pos});
 }
